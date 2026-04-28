06d33da8d9549a4c2f50ad13d829c606f0e5f4b6
Niharika Khare <niharikakhare2101@gmail.com>
2026-04-28 22:46:33 +0530

    cat implementation

---
+++ commands/cat.c
@@ -0,0 +1,49 @@
+#include "common.h"
+#include "cat.h"
+
+/* command flags */
+#define B_                  1
+#define N_                  2
+
+#define MAX_FILE_COUNT      20
+
+static void parse_params(int *flags, char ** argv, char ** f_list) {
+    while (*++argv) {
+        if (strcmp(*argv, "-b") == 0) {
+            *flags = *flags | B_;
+        }
+        else if (strcmp(*argv, "-n") == 0) {
+            *flags = *flags | N_;
+        } 
+        else {
+            *f_list++ = *argv;
+        } 
+    }
+    *f_list = NULL;
+}
+
+int main(int argc, char ** argv) {
+
+    int flags, exit_status = 0;
+    char *f_list[MAX_FILE_COUNT];
+
+    parse_params(&flags, argv, f_list);
+
+    for (int i=0; f_list[i]; i++) {
+        int fd = open(f_list[i], O_RDONLY);
+
+        if (fd == -1) {
+            log_err("err: cat: unable of open file: %s\n", f_list[i]);
+            exit_status = 1;
+            continue;
+        }
+        char buffer[2048];
+        int bytes = 0;
+        while ((bytes = read(fd, buffer, sizeof (buffer)))) {
+            write(STDOUT_FILENO, buffer, bytes);
+        }
+
+        close(fd);
+    }
+    return exit_status;
+}
---
+++ include/commands/cat.h
@@ -0,0 +1,14 @@
+#ifndef _CAT_H_
+#define _CAT_H_
+
+#include <fcntl.h>
+#include <string.h>
+
+/* command flags */
+#define B_                  1
+#define N_                  2
+
+#define MAX_FILE_COUNT      20
+
+
+#endif /** _CAT_H_ */
