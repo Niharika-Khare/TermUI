1775ee2159dbd88ab039f012fd9e360fd7c80315
Niharika Khare <niharikakhare2101@gmail.com>
2026-05-07 11:29:24 +0530

    Scrollable history with less number of variables

--- src/choices/history.c
+++ src/choices/history.c
@@ -3,14 +3,14 @@
 #include "terminal.h"
 
 static char history_buffer [MAX_APP_HISTORY_CNT] [MAX_ENTRY_LEN];
-static int head = 0, hist_cnt = 0;
+static int head = 0, hist_cnt = 0 ;
 
-static inline void format_entry(char * entry) {
+static inline void format_entry (char * entry) {
 
-    int len = strlen(entry);
-    for (int i=len-1; i>=0; i--) {
+    int len = strlen (entry) ;
+    for ( int i = len-1; i >= 0; i-- ) {
 
-        if (entry[i] == '\n') {
+        if ( entry[i] == '\n' ) {
             entry[i] = '\0';
         } 
         else {
@@ -19,78 +19,68 @@ static inline void format_entry(char * entry) {
     }
 }
 
-void init_history() {
-    memset ( history_buffer, '\0', sizeof(history_buffer) ) ;
+void init_history () {
+    memset (history_buffer, '\0', sizeof (history_buffer)) ;
 }
 
 
 void save_history (char * entry) {
      
-    format_entry(entry) ;
-    strcpy ( history_buffer[head], entry ) ;
-    head = (head + 1) % MAX_APP_HISTORY_CNT;
-    hist_cnt++;
+    format_entry (entry) ;
+    strcpy (history_buffer[head], entry) ;
+    head = (head + 1) % MAX_APP_HISTORY_CNT ;
+    hist_cnt++ ;
 }
 
-int view_history() {
+int view_history () {
 
     Winsize w;
-    get_window_size(&w);
+    get_window_size (&w);
     
-    char * print_format = DISABLE_LINE_WRAP "%3d. %s\n" ENABLE_LINE_WRAP;
-    char print_buffer [MAX_APP_HISTORY_CNT] [MAX_ENTRY_LEN + sizeof (print_format) + 1];
+    POS st = { 1, min (hist_cnt, w.ws_row) } ;
+    int offset = 0 ;
 
-    POS st;
-    st.c_vert =  min(hist_cnt, w.ws_row);
-    st.c_horz =  1;
     do {
-        int ind = hist_cnt > w.ws_row ? hist_cnt - w.ws_row : 0;
-        memset ( print_buffer, '\0', sizeof(print_buffer) ) ;
-
+        int ind = hist_cnt > MAX_APP_HISTORY_CNT ? hist_cnt - MAX_APP_HISTORY_CNT + 1: 1;
         int tail = head;
-        int  p_i = 0, p_st = 0;
-
+        int count = 0;
+        
+        clear_terminal ();
         do {
             if (history_buffer[tail][0] != '\0' ) {
-                
-                snprintf ( print_buffer [p_i], 
-                       MAX_ENTRY_LEN + sizeof (print_format) + 1, 
-                       print_format,
-                       ind + 1, 
-                       history_buffer[tail]) ;
 
-                if (p_i > w.ws_row) {
-                    p_st++ ;
-                } 
+                const char * fmt = (count == 0) ? DISABLE_LINE_WRAP "%3d. %s" ENABLE_LINE_WRAP
+                                               : DISABLE_LINE_WRAP "\n%3d. %s" ENABLE_LINE_WRAP ;
+                dprintf (STDOUT_FILENO, fmt, ind + offset, history_buffer [tail + offset] );
+                
                 ind++ ;
-                p_i++ ;
+                count++ ;
             }
 
             tail = (tail + 1) % MAX_APP_HISTORY_CNT;
         }
-        while ( tail != head );
-
-    
-        clear_terminal();
-        write ( STDOUT_FILENO , 
-                print_buffer + p_st , 
-                (p_i - p_st + 1) * (MAX_ENTRY_LEN + sizeof (print_format) + 1) ) ;
-
+        while ( tail != head ) ;
     
         relocate_cursor (st.c_vert, 1) ;
-        st = cursor_scroll (st, p_i, 0) ;
+        st = cursor_scroll (st, count, 0) ;
 
         if (st.c_vert == SCROLL_ONE_DOWN_CODE) {
-            st.c_vert = min (p_i, w.ws_row);
-            // p_st = p_st < p_i - st.c_vert ? p_st + 1 : p_st ;
+
+            if (count > w.ws_row) {
+                st.c_vert = w.ws_row ;
+                offset = (offset < 0) ? offset + 1 : offset;
+            } 
+            else {
+                st.c_vert = count;
+            } 
         }
         else if (st.c_vert == SCROLL_ONE_UP_CODE) {
             st.c_vert = 1;
-            // p_st = p_st > 0 ? p_st - 1 : p_st ; 
+            offset = (offset > w.ws_row - count) ? offset - 1: offset;
         }
         relocate_cursor (1, 1) ;
     }
-    while (st.c_vert != ESCAPE_CODE && st.c_vert != QUIT_CODE);
+    while (st.c_vert != ESCAPE_CODE && st.c_vert != QUIT_CODE) ;
 
     return st.c_vert;
 }
